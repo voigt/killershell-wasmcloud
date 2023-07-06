585b5eaa6d63d40eeb7db79002e159ec5909dd67
Christoph Voigt <voigt.christoph@gmail.com>
2023-07-06 09:16:47 +0200

    stop k8s services in setup

--- 01-wasmcloud-intro/setup.sh
+++ 01-wasmcloud-intro/setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Stop k8s services
+systemctl daemon-reload
+systemctl stop kubepods.slice
+systemctl stop kubelet
+
 curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | bash
 apt install wash openssl direnv
 
 cat << EOF > .envrc
 export WASMCLOUD_CLUSTER_SEED="SCADMX4TAR3KWXQT2VI66WVJIEGSORZ46DIKQOVU7EUIVX6V7CQ7QRHWMM"
+EOF
+
+cat << EOF >> .bashrc
+
+eval "\$(direnv hook bash)"
 EOF
 
 direnv allow
