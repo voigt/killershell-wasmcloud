6bf04ea4e5681d78edb8888bf6706925a8c2db63
Sven Pfennig <s.pfennig@reply.de>
2023-07-11 13:21:11 +0200

    avoid line breaks in foreground script when printing dots

--- 00-wasmcloud-playground/02_foreground_setup.sh
+++ 00-wasmcloud-playground/02_foreground_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing scenario..."
-while [ ! -f /tmp/finished ]; do sleep 1 && echo "."; done
+while [ ! -f /tmp/finished ]; do sleep 1 && echo -n "."; done
 echo DONE
 
 direnv allow
